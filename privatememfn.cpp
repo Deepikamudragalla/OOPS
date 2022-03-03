6e839176efbe18b952ddd69034539b88c8127edb
Deepikamudragalla <79035068+Deepikamudragalla@users.noreply.github.com>
2022-03-03 22:40:44 +0530

    Create privatememfn.cpp

---
+++ privatememfn.cpp
@@ -0,0 +1,22 @@
+// private member functions : these are private unlike other member functions which are mostly public,these cant be accessed with dot operator can only be accessed with member fns
+
+#include<iostrem>
+using namespace std;
+class sample
+{
+  int m;
+  void read(void)
+  public :
+  void display(void)
+ }
+ void sample :: display()
+ {
+  read();
+  } //legal
+  
+  int main()
+  {
+  sample A;
+  A.read();//illegal }
+  
+  
